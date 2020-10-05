f205663fad09c5511ac9167a8f41631f677bc3a2
Ali <aliwolfi18@gmail.com>
2020-10-05 02:03:13 -0700

    init git

---
+++ script.js
@@ -0,0 +1,83 @@
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+
+// calculate first and second values depending on operator (with keys)
+const calculate = {
+    '/': (firstNumber, sendNumber) => firstNumber / sendNumber,
+    '*': (firstNumber, sendNumber) => firstNumber * sendNumber,
+    '-': (firstNumber, sendNumber) => firstNumber - sendNumber,
+    '+': (firstNumber, sendNumber) => firstNumber + sendNumber,
+    '=': (firstNumber, sendNumber) => sendNumber
+};
+
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
+function sendNumberValue(number) {
+    // replace current display value if first value is entered
+    if (awaitingNextValue) {
+        calculatorDisplay.textContent = number;
+        awaitingNextValue = false;
+    } else {
+        //if current display value is 0 , replace it , if not add number
+        const displayValue = calculatorDisplay.textContent;
+        calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
+}
+
+function addDecimal() {
+    // if operator pressed, don't add decimal
+    if (awaitingNextValue) return;
+    // if no decimal, add one
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
+    }
+}
+
+function useOperator(operator) {
+    const currentValue = Number(calculatorDisplay.textContent);
+    // prevent multiple operators (return in the end it's mean that don't run rest of function below)
+    if (operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return;
+    }
+    ;
+    // assign firstValue if no value
+    if (!firstValue) {
+        firstValue = currentValue;
+    } else {
+        // square bracket to assign a key and then in brackets we pass two parameters
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        calculatorDisplay.textContent = calculation;
+        // it's interesting
+        firstValue = calculation;
+    }
+    // ready for next value, store operator
+    awaitingNextValue = true;
+    operatorValue = operator;
+}
+
+// reset all values display
+function resetAll() {
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
+    calculatorDisplay.textContent = '0';
+}
+
+// add event listeners for numbers, operator, decimal buttons
+inputBtns.forEach((inputBtn) => {
+    if (inputBtn.classList.length === 0) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('operator')) {
+        inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
+    } else if (inputBtn.classList.contains('decimal')) {
+        // when u use arrow function u need to call that function as well
+        inputBtn.addEventListener('click', () => addDecimal());
+    }
+});
+
+// event listener
+clearBtn.addEventListener('click', () => resetAll());
